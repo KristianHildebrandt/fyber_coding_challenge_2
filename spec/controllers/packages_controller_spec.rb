ef11b3ea0ef3524b363bf5ae288f876ad145afc5
Kristian Hildebrandt <kristian@Kristians-MacBook-Pro.local>
2015-10-14 17:28:31 +0200

    test for packages#index action

--- spec/controllers/packages_controller_spec.rb
+++ spec/controllers/packages_controller_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe PackagesController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "gets all the packages" do
+      allow(Package).to receive(:all).and_return('foo')
+
+      get :index
+      expect(assigns(:packages)).to eq('foo')
+    end
   end
 
 end
